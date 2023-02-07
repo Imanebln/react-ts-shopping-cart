783061d6a3261d01528443026fa7a104b42629c4
Imanebln <boulouane.imane@gmail.com>
2023-02-07 23:24:04 +0100

    added store and shopping cart

---
+++ src/pages/Store.tsx
@@ -0,0 +1,44 @@
+import StoreItems from '../data/items.json'
+import {Col, Row, Dropdown} from 'react-bootstrap';
+import { StoreItem } from '../components/StoreItem';
+import { useEffect, useState } from 'react';
+
+export function Store(){
+
+    const [data, setData] = useState(StoreItems);
+
+    const handleSort = (sortType) => {
+        const sortedData = [...data].sort((a, b) => {
+          if (sortType === 'asc') {
+            return a.price - b.price;
+          } else {
+            return b.price - a.price;
+          }
+        });
+    
+        setData(sortedData);
+      };
+
+    return (
+    <>
+        {/* <h1>Our Store</h1> */}
+        <Dropdown className='mb-3'>
+            <Dropdown.Toggle variant="outline-success" id="dropdown-basic">
+                 Sort by Price
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item onClick={() => handleSort("desc")}>Higher to Low</Dropdown.Item>
+                <Dropdown.Item onClick={() => handleSort("asc")}>Lower to High</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+        <Row md={2} xs={1} lg={3} className="g-3">
+            {data.map(item => (
+               <Col>
+                  <StoreItem key={item.id}  {...item} />
+               </Col> 
+            ))}
+            
+        </Row>
+    </>
+    ) 
+}
